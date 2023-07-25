cde4551922cf805afd543b3f9876c6580c00393e
yasminsheehan <89693741+yasminsheehan@users.noreply.github.com>
2023-07-26 00:07:08 +0100

    Calendar JS

--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
-
 function yasmiesFunction() {
     console.log("hellobarto");
     document.getElementById("calendar").innerHTML = "Birthdays";
  }
 
- function hideCalendar() {
-    var calendar = document.getElementById("springTable");
+//  function test() {
+//     var calendar = document.getElementById("springTable");
 
-    if (calendar.style.visibility === "hidden") 
-    {
-      calendar.style.visibility = "visible";
-    }else{
-      calendar.style.visibility = "hidden";
-    }
+//     if (calendar.style.visibility === "visible") 
+//     {
+//       calendar.style.visibility = "hidden";
+//     }
 
- }
+//  }
 
+function test(text) {
+   console.log("season "+text);
+
+   document.getElementById("springTable").style.visibility = "hidden";
+   document.getElementById("summerTable").style.visibility = "hidden";
+   document.getElementById("autumnTable").style.visibility = "hidden";
+   document.getElementById("winterTable").style.visibility = "hidden";
+
+   var header = document.getElementById("calendar");
+   var calendar = document.getElementById("springTable");
+
+   if (text == "spring"){ 
+      // console.log("growth");
+      header.innerHTML = "Growth";
+      document.getElementById("springTable").style.visibility = "visible";
+   }
+
+   if (text == "summer"){ 
+   //  console.log("warmth");
+    header.innerHTML = "Warmth";
+    document.getElementById("summerTable").style.visibility = "visible";
+   }
+
+   if (text == "autumn"){ 
+   //  console.log("change");
+    header.innerHTML = "Change";
+    document.getElementById("autumnTable").style.visibility = "visible";
+   }
+
+   if (text == "winter"){ 
+   //  console.log("snow");
+    header.innerHTML = "Snow";
+    document.getElementById("winterTable").style.visibility = "visible";
+   }
+}
+
+
+// HTML                          JAVA
+// style="width : 20px"          calendar.style.width = "20px";
